46dc5f1a5bb43e94fbcf9ca50237b9b2b06d4621
Jakob <jgaardsted@gmail.com>
2022-12-04 09:09:48 +0100

    ch24fixes

--- src/O5ai/24SpellAI.ts
+++ src/O5ai/24SpellAI.ts
@@ -7,16 +7,18 @@ import { GameIF } from "O3build/08GameIF";
 import { CanSee } from "O4cmds/18CanSee";
 import { BuffCmd } from "O4cmds/24BuffCmd";
 import { MobAiIF } from "./10MobAiIF";
+import { MobAI2_cat } from "./11MobAI2_cat";
+import { MobAI3_ant } from "./14MobAi3_ant";
 import { SleepAI } from "./18SleepAI";
 
-// from 18WakeAI
 export class SpellAI implements MobAiIF {
   constructor(public speed:number, 
-              public spellRate:number) {}
-  ..
+              public spellRate:number) {}  
+  aiDir:MobAiIF = new MobAI2_cat();
+  aiRnd:MobAiIF = new MobAI3_ant();
   turn(me:Mob, enemy:Mob, game:GameIF):boolean {
     if (this.maybeCastSpell(me,enemy,game)) {
-        return true;
+      return true;
     }
     let r = game.rnd; 
     for (let i=0;i<this.speed;++i) {
@@ -25,9 +27,9 @@ export class SpellAI implements MobAiIF {
     }
     let far = SleepAI.isNear(me,enemy);
     if (far) { 
-        me.mood = 
-            r.oneIn(3) ? Mood.Asleep : Mood.Wake;
-    }    
+      me.mood = 
+        r.oneIn(3) ? Mood.Asleep : Mood.Wake;
+    }   
     return true;
   }
 
@@ -44,7 +46,7 @@ export class SpellAI implements MobAiIF {
   pickBuff(me:Mob, r:Rnd):Buff { return Buff.Confuse; }
   
   cast(buff:number, me:Mob, 
-      enemy:Mob, game:GameIF):boolean {
+       enemy:Mob, game:GameIF):boolean {
     let spell = new BuffCmd(buff,enemy,game);
     return spell.exc();
   }
