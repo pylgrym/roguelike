b95146dbf08b052886c8992b26a946f515f808e5
Jakob Gaardsted <jgaardsted@gmail.com>
2022-12-20 19:16:55 +0100

    confuse-fix

--- src/O4cmds/11MoveBumpCmd.ts
+++ src/O4cmds/11MoveBumpCmd.ts
@@ -5,19 +5,30 @@ import { GameIF } from "O3build/08GameIF";
 import { CmdBase } from "./09CmdBase";
 import { MoveCmd } from "./09MoveCmd";
 import { HitCmd } from "./11HitCmd";
+import { Able } from "./25Able";
+import { Act } from "./25Act";
 
 export class MoveBumpCmd extends CmdBase {
   constructor(
     public dir:WPoint, public me:Mob, public g:GameIF
   ) { super(me,g); }  
+  
+  able(m:Mob, g:GameIF,act:Act):Able { // ch25
+    return { able:true, turn:false }
+  } // (For MoveBumpCmd, each sub-cmd checks instead.)
+
   exc():boolean { 
-    let np = this.dir.plus(this.me.pos);
-    let map = <DMapIF> this.g.curMap();
+    let g=this.g;
+    let m=this.me;
+    this.confused(g,this.dir);
+
+    let np = this.dir.plus(m.pos);
+    let map = <DMapIF> g.curMap();
     if (!map.legal(np)) { return false; }
     let cell = map.cell(np);     
     let cmd = cell.mob  
-      ? new  HitCmd(this.me,cell.mob,this.g) 
-      : new MoveCmd(this.dir,this.me,this.g);
+      ? new  HitCmd(m,cell.mob,g) 
+      : new MoveCmd(this.dir,m,g);
     return cmd.turn();
   }
 }
