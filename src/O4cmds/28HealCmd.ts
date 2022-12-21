81e55ddea28b3c698fea51cd8286214bc6489026
jakobgaardsted <jg@ajoursystem.dk>
2022-12-21 09:28:37 +0100

    bob

--- src/O4cmds/28HealCmd.ts
+++ src/O4cmds/28HealCmd.ts
@@ -2,25 +2,24 @@ import { HealthAdj } from "./11HealthAdj";
 import { CmdBase } from "./09CmdBase";
 import { Mob } from "O2model/09Mob";
 import { GameIF } from "O3build/08GameIF";
-//import { GameIF } from "O2model/08GameIF";
 
 export class HealCmd extends CmdBase {
-    constructor(
-        public r:number, 
-        public me:Mob, public g:GameIF
-    ) { super(me,g); }
-    exc(): boolean {
-        let g = this.g;
-        let R = g.rnd;
-        let r = this.r;
-        let a = Math.ceil( r*0.5);
-        let b = Math.floor(r*1.5);
-        let hp = R.rnd(a,b);
-        console.table({a,b,hp});       
-        
-        HealthAdj.heal(this.me, hp); 
-        let msg = `${this.me.name} feels better (${hp})`;
-        g.msg(msg);
-        return true; 
-    }
+  constructor(
+      public r:number, 
+      public me:Mob, public g:GameIF
+  ) { super(me,g); }
+  exc(): boolean {
+    let g = this.g;
+    let R = g.rnd;
+    let r = this.r;
+    let a = Math.ceil( r*0.5);
+    let b = Math.floor(r*1.5);
+    let hp = R.rnd(a,b);
+    console.table({a,b,hp});       
+    
+    HealthAdj.heal(this.me, hp); 
+    let msg = `${this.me.name} feels better (${hp})`;
+    g.msg(msg);
+    return true; 
+  }
 }
--- src/O4cmds/28PortCmd.ts
+++ src/O4cmds/28PortCmd.ts
@@ -3,34 +3,33 @@ import { WPoint } from "O2model/07WPoint";
 import { DMapIF } from "O2model/07DMapIF";
 import { Mob } from "O2model/09Mob";
 import { GameIF } from "O3build/08GameIF";
-//import { GameIF } from "O2model/08GameIF";
 
 export class PortCmd extends CmdBase {
-    constructor(
-        public r:number, public me:Mob, public g:GameIF
-    ) { super(me,g); }
-    exc(): boolean {
-        let g = this.g;
-        let map = <DMapIF> g.curMap();
-        let p = this.pick(this.me.pos, this.r, map);
-        if (!p) { return false; }
-        map.moveMob(this.me,p);        
-        g.msg(`${this.me.name} shimmers`);
-        return true; 
-    }
-    pick(c:WPoint,r:number,map:DMapIF):WPoint|null {
-        let R = this.g.rnd;
-        let p = new WPoint();
-        for (let ix=15; ix>0; ) {
-            let dx=R.rnd(-r,r), dy = R.rnd(-r,r);
-            p.x = c.x + dx;
-            p.y = c.y + dy;
-            if (!map.legal(p)) { continue; }
-            --ix;
-            if (map.cell(p).blocked()){continue;}
-            console.table({r,dx,dy});
-            return p;
-        } 
-        return null;
-    }
+  constructor(
+      public r:number, public me:Mob, public g:GameIF
+  ) { super(me,g); }
+  exc(): boolean {
+    let g = this.g;
+    let map = <DMapIF> g.curMap();
+    let p = this.pick(this.me.pos, this.r, map);
+    if (!p) { return false; }
+    map.moveMob(this.me,p);        
+    g.msg(`${this.me.name} shimmers`);
+    return true; 
+  }
+  pick(c:WPoint,r:number,map:DMapIF):WPoint|null {
+    let R = this.g.rnd;
+    let p = new WPoint();
+    for (let ix=15; ix>0; ) {
+      let dx=R.rnd(-r,r), dy = R.rnd(-r,r);
+      p.x = c.x + dx;
+      p.y = c.y + dy;
+      if (!map.legal(p)) { continue; }
+      --ix;
+      if (map.cell(p).blocked()){continue;}
+      console.table({r,dx,dy});
+      return p;
+    } 
+    return null;
+  }
 }
