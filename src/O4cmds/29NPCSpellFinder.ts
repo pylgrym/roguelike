9d2094d71740ab6b2d38569dd001bc78de6edd87
Jakob Gaardsted <jgaardsted@gmail.com>
2022-12-28 20:25:06 +0100

    fixgameIFbuilder2

---
+++ src/O4cmds/29NPCSpellFinder.ts
@@ -0,0 +1,65 @@
+import { HealCmd } from './28HealCmd';
+import { GameIF } from "O3build/08GameIF";
+import { CmdIF } from './09CmdIF';
+import { PortCmd } from './28PortCmd';
+import { Stack } from 'O1term/05ScreenStack';
+import { MakerIF } from 'O6screen/06ScreenMakerIF';
+import { SScreenIF } from 'O1term/05SScreenIF';
+import { Spell } from './29Spell';
+import { CostIF } from './28CostIF';
+import { BuffCmd } from './24BuffCmd';
+import { Buff } from 'O2model/24BuffEnum';
+import { Mob } from 'O2model/09Mob';
+import { CleanseAllCmd } from './29CleanseCmd';
+import { MultiplyCmd } from './29MultiplyCmd';
+import { SummonCmd } from './29SummonCmd';
+
+export class NPCSpellFinder {
+  ply:Mob;
+  constructor(public g:GameIF, public ss:Stack,
+              public maker:MakerIF) 
+  { this.ply = g.ply; }
+  find(me:Mob,spell:Spell,cost:CostIF|undefined): CmdIF|SScreenIF|null {
+    let g = this.g;
+    let level = 1;
+    var s:SScreenIF|undefined;
+    var cmd:CmdIF;
+
+    switch (spell) {
+    case Spell.Heal:    cmd = new HealCmd(level,me,g); break;
+    case Spell.D_Charm:   cmd = this.buff(me,Buff.Charm    ); break;
+    case Spell.D_Slow:    cmd = this.buff(me,Buff.Slow     ); break;
+    case Spell.D_Afraid:  cmd = this.buff(me,Buff.Afraid   ); break;
+    //case Spell.Missile: s = this.dir(cmd = new BulletCmd(g.ply,g,this.ss,this.maker)); break;
+    case Spell.D_Poison:  cmd = this.buff(me,Buff.Poison   ); break;
+    case Spell.D_Confuse: cmd = this.buff(me,Buff.Confuse  ); break;
+    case Spell.D_Silence: cmd = this.buff(me,Buff.Silence  ); break;
+    case Spell.Cleanse: cmd = new CleanseAllCmd(me,g); break;
+    case Spell.D_Stun:    cmd = this.buff(me,Buff.Stun     ); break;
+    case Spell.D_Burn:    cmd = this.buff(me,Buff.Burn     ); break;
+    case Spell.D_Blind:   cmd = this.buff(me,Buff.Blind    ); break;
+    case Spell.Multiply: cmd = new MultiplyCmd(me,g); break;
+    case Spell.D_Freeze:  cmd = this.buff(me,Buff.Freeze   ); break;
+    case Spell.D_Root:    cmd = this.buff(me,Buff.Root     ); break;
+    case Spell.D_Shock:   cmd = this.buff(me,Buff.Shock    ); break;
+    case Spell.Port:    cmd = new PortCmd(6,me,g); break;
+    case Spell.D_Paralyze:cmd = this.buff(me,Buff.Paralyze ); break;
+    case Spell.D_Sleep:   cmd = this.buff(me,Buff.Sleep    ); break;
+    case Spell.D_Petrify: cmd = this.buff(me,Buff.Petrify  ); break;
+    case Spell.Summon: cmd = new SummonCmd(me,g); break;
+    case Spell.D_Bleed:   cmd = this.buff(me,Buff.Bleed    ); break;
+    case Spell.D_Levitate:cmd = this.buff(me,Buff.Levitate ); break;
+    case Spell.D_Disarm:  cmd = this.buff(me,Buff.Disarm   ); break;
+    default: return null; 
+    }
+    cmd.setCost(cost);
+    return s ? s : cmd;
+  }   
+  buff(me:Mob,buff:Buff): CmdIF {
+    return new BuffCmd(buff,this.ply,this.g,me);
+  }
+  //payload(inner:CmdIF,me:Mob):CmdOrScreen {
+  //  let cmd:CmdIF = new PayloadCmd(me,this.g,this.ss,this.maker,inner);
+  //  return {cmd:cmd,s:undefined};
+  //}
+ }
