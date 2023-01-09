4c17db80f60a92cbaf2d3f4371fba63169607db9
jakobgaardsted <jg@ajoursystem.dk>
2023-01-09 15:24:42 +0100

    orig dung back

--- src/O2model/13Dung.ts
+++ src/O2model/13Dung.ts
@@ -1,23 +1,16 @@
 import { GameIF } from "O3build/08GameIF";
-import { BuildIF4 } from "O3build/29BuildIF3";
 import { DMapIF } from "./07DMapIF";
 import { WPoint } from "./07WPoint";
-import { GlyphMap1 } from "./16GlyphInf1";
 
 export class Dung {
   level:number = 0;
   maps:DMapIF[] = [];
-  dragonLevel:number = -1; // ch30
-  
   curMap(g:GameIF): DMapIF {
       return this.getLevel(this.level,g);
   }
   getLevel(L:number, g:GameIF): DMapIF {
     if (!this.hasLevel(L)) { 
-      // ch30:
-      //let map = g.build.makeLevel(g.rnd,L,this.dragonLevel);
-      let b = <BuildIF4> g.build;
-      let map = b.makeDragonLevels(g.rnd,L,this.dragonLevel);
+      let map = g.build.makeLevel(g.rnd,L);
       this.add(map,L);
     }
     return this.maps[L];
@@ -35,11 +28,6 @@ export class Dung {
       let ply = g.ply;
       this.curMap(g).removeMob(ply); 
       this.level = newLevel;
-      this.curMap(g).enterMap(ply,np);
-      this.dragonStyle();
-  }
-  dragonStyle() {
-    let hasDragon = (this.level == this.dragonLevel);
-    GlyphMap1.initGlyphs(hasDragon);
+      this.curMap(g).enterMap(ply,np);      
   }
 }
