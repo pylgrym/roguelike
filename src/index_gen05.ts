2b7ac606703b58ceeac69a1b5b8f2df0ccf95f38
Jakob Gaardsted <jgaardsted@gmail.com>
2023-01-18 23:19:22 +0100

    x

--- src/index_gen05.ts
+++ src/index_gen05.ts
@@ -1,6 +1,13 @@
+import { Glyph } from "O2model/07Glyph";
+import { Rnd } from "O2model/07Rnd";
+import { WPoint } from "O2model/07WPoint";
+import { MapDrawer } from "O7mapgen/33MapDrawer";
+import { G3_GridBox_Algo } from "O7mapgen/G3_GridBox_Algo";
+import { G4_Maze_Algo } from "O7mapgen/G4_Maze_Algo";
+
 let dm = new MapDrawer( new WPoint(71,55), Glyph.Wall );
 let rnd = new Rnd(17);
 var algoBox = new G3_GridBox_Algo(dm);
-var algoMaze = new Maze_Algo(dm, rnd, dm.dim);
+var algoMaze = new G4_Maze_Algo(dm, rnd, dm.dim);
 algoBox.run(rnd,dm.dim, 2);
 algoMaze.run(rnd);
